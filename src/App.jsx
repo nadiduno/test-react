9c52f06589876ab7130586bce1de23861396a6fa
Nadi Duno <nadiduno@gmail.com>
2023-01-25 23:29:31 -0300

    CSS module

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header'
 import { Post}  from './components/Post'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author = 'Nadi Duno'
         content = 'Developer'
