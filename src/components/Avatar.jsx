db8d6e54a97eac1de72d8bd9472953a2a8d9d729
Nadi Duno <nadiduno@gmail.com>
2023-01-30 16:47:39 -0300

    Rebilled the code

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,14 +1,14 @@
 import styles from './Avatar.module.css'
 
 //Desestructurando os argumento em lugar de receber Avatar(props){} tenho que
-//props.src se guarda em src
-//props.hasBorder se guarda em hasBorder
+//Uso src para almacenar props.src
+//hasBorder para almacenar props.hasBorder
 //por isso Avatar({src,hasBorder}){}
 //Agora bem, em caso de não recever as propiedades e para que o programa não de erro
 //Coloquei uma Features de ES6 chamada "Defaut Parameters" no argumento
-//A qual consiste em asignar um valor por defecto a o argumento em caso de não recever nada
-//Em caso de não recever a props.src colocaremos por defecto um "avatar predefinido" que esta na rota src/assets/avatarcomicsneon.jpg
-//Em caso de não recever a props.hasBorder colocaremos por defecto "true" 
+//A qual consiste em asignar um valor por default ao argumento em caso de não recever nada
+//Em caso de não recever a props.src colocaremos por default um "avatar predefinido" que esta na rota src/assets/avatarcomicsneon.jpg
+//Em caso de não recever a props.hasBorder colocaremos por default "true" 
 //Por isso Avatar({src="src/assets/avatarcomicsneon.jpg", hasBorder=true}){}
 
 export function Avatar({src="src/assets/avatarcomicsneon.jpg", hasBorder=true}){
