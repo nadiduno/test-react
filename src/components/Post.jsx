ef3715136e8068d6827fd10826d841702589edd3
Nadi Duno <nadiduno@gmail.com>
2023-01-29 22:13:01 -0300

    Component Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,13 @@ export function Post(){
           <a href="#">#FrontEnd</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentario"/>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
